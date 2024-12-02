c07076c0be3f929a44d33243d3405ff0a0d64055
ernisto <joaovitordesouza707@gmail.com>
2024-12-02 20:59:03 -0300

    fix: put "powershell" prefix on publish cmd

--- release.config.js
+++ release.config.js
@@ -14,7 +14,7 @@ const config = {
         }],
         ["@semantic-release/exec", {
             prepareCmd: "python scripts/python/set-wally-version.py ./wally.toml ${nextRelease.version}",
-            publishCmd: "scripts/wally-publish.cmd"
+            publishCmd: "powershell ./scripts/wally-publish.cmd"
         }]
     ]
 }
