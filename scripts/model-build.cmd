5fdb7884ee2fd2733dc7c6e25d354f5720d9bbdc
ernisto <joaovitordesouza707@gmail.com>
2024-12-03 00:12:38 -0300

    build: fix mkdir out/rojo

--- scripts/model-build.cmd
+++ scripts/model-build.cmd
@@ -1,7 +1,7 @@
 mkdir Packages
 echo --!nocheck> Packages/init.luau
 echo return require(script.package) >> Packages/init.luau
-
 call scripts/wally-install.cmd model.project.json &
-mkdir out/rojo
+
+mkdir out & cd out & mkdir rojo & cd .. &
 rojo build model.project.json --output out/rojo/model.rbxm
