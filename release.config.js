0fb8efa71354f84be19d216f718b2fdddac01eb6
ernisto <joaovitordesouza707@gmail.com>
2024-12-02 21:00:49 -0300

    ci: fix asset to expose model.rbxm instead of .rbxl

--- release.config.js
+++ release.config.js
@@ -4,12 +4,12 @@ const config = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         ['@semantic-release/git', {
-            assets: ["out/rojo/model.rbxl"],
+            assets: ["out/rojo/model.rbxm"],
             message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         ["@semantic-release/github", {
             assets: [
-                { path: "out/rojo/model.rbxl", label: "Rojo build" },
+                { path: "out/rojo/model.rbxm", label: "Rojo build" },
             ]
         }],
         ["@semantic-release/exec", {
