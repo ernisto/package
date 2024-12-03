d85248486f989e933f534f307f46c87d770eb740
ernisto <joaovitordesouza707@gmail.com>
2024-12-03 00:33:28 -0300

    build(model): generate Packages/init only when build model

--- scripts/model-build.cmd
+++ scripts/model-build.cmd
@@ -1,6 +1,3 @@
-mkdir Packages
-echo --!nocheck> Packages/init.luau
-echo return require(script.package) >> Packages/init.luau
 call scripts/wally-install.cmd model.project.json &
 
 mkdir out & cd out & mkdir rojo & cd .. &
